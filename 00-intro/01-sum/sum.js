5497b2f369f68609c47f52d88c5ce91c5ee77e5e
javascriptru <67475495+jsru-1@users.noreply.github.com>
2021-10-28 10:12:33 +0300

    Added teacher solution for task: Сумма двух чисел

---
+++ 00-intro/01-sum/sum.js
@@ -0,0 +1,10 @@
+/**
+ * Функция, возвращающая сумму двух чисел
+ *
+ * @param {number} a - первое число
+ * @param {number} b - второе целое
+ * @return {number} сумма чисел a и b
+ */
+export function sum(a, b) {
+  return a + b;
+}
