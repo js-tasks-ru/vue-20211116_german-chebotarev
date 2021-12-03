b417d22c9c37eff6f28ae0c46a81d80dfe9d82b0
sv-yaz-ist <german.chebotarev@gmail.com>
2021-12-03 17:13:18 +0300

    basics 1,2,3,4

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,29 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+  data() {
+    return {
+      firstValue: 1,
+      secondValue: 1,
+      operation: 'sum',
+    };
+  },
+  computed: {
+    calcResult() {
+      switch (this.operation) {
+        case 'sum':
+          return this.firstValue + this.secondValue;
+        case 'subtract':
+          return this.firstValue - this.secondValue;
+        case 'multiply':
+          return this.firstValue * this.secondValue;
+        case 'divide':
+          if (this.secondValue !== 0) {
+            return this.firstValue / this.secondValue;
+          } else return 'Делить на ноль нельзя!';
+        default:
+          return '';
+      }
+    },
+  },
+}).mount('#app');
